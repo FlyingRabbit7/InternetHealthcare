ff7d5acc2a256bb837b65dca2bfce093779477b6
tongshengzhi <tongshengzh1@163.com>
2023-05-17 16:10:25 +0800

    输入框

---
+++ src/components/InputArea/InputArea.js
@@ -0,0 +1,26 @@
+import "./InputArea.css";
+import React, { useState } from "react";
+import { Input } from "antd";
+
+const { TextArea } = Input;
+
+function InputArea() {
+  const [value, setValue] = useState('');
+  const send = () => {
+    console.log(value)
+  };
+  return (
+    <div className="container">
+      <div className="input-wrap">
+        <div className="input-area">
+          <TextArea className="text-area" value={value} onChange={(e) => setValue(e.target.value)} placeholder="你可在此输入进行回答" autoSize={{ minRows: 1, maxRows: 4 }} />
+        </div>
+        <div className="btn-area" onClick={send}>
+          <div>发送</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InputArea;
